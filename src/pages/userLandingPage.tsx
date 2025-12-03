a65ca0b7d6c069f3e4f2c0a6d7c8d930b724401c
Sangeeth Ramesh <sangeethramesh05@gmail.com>
2025-12-04 00:27:36 +0530

    first commit

--- src/pages/userLandingPage.tsx
+++ src/pages/userLandingPage.tsx
@@ -11,7 +11,7 @@ export default function BirthdayApp() {
 
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (name.toLowerCase() === 'anuvinda' && dob === '2002-12z-04') {
+    if (name.toLowerCase() === 'anuvinda' && dob === '2002-12-04') {
       setIsLoggedIn(true);
       setError('');
     } else {
